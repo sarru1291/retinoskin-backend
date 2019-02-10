70a600ded91d4747bc4e04edc0293e59b45cab45
sarru1291 <sarru1291@gmail.com>
2019-02-11 02:27:19 +0530

    user authentication token  added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app=express();
 const path=require('path');
 require('./config/config');
 const imageTest=require('./routers/imageTest/imageTest');
-// const users=require('./routers/users/users');
+const users=require('./routers/users/users');
 const port=process.env.PORT || 8086;
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use('/api',imageTest);
-// // app.use('/users',users);
+app.use('/users',users);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/build/index.html'));
