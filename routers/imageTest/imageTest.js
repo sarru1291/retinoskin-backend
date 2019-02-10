f298d1b02bf859e3e74a6393d3a3f267a419c81a
sarru1291 <sarru1291@gmail.com>
2019-02-11 01:54:16 +0530

    image upload api added

---
+++ routers/imageTest/imageTest.js
@@ -0,0 +1,100 @@
+const express=require('express');
+const router=express.Router();
+const cloudinary=require('cloudinary');
+const formidable=require('formidable');
+const axios=require('axios');
+require('../../config/config');
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET 
+});
+
+function uploadImage(files) {
+  return new Promise((resolve,reject)=>{
+    cloudinary.v2.uploader.upload(
+      files.image.path, 
+      {folder:"retinoskin/notLoggedInUsers/"},
+      function (error,result) {
+        if(result){
+          resolve(result)
+        }else{
+          reject()
+        }
+      });
+  });
+}
+function modelOutput(imageURL,option) {
+    try {
+      return axios.post('https://retinoskin-model.herokuapp.com/api/model/test',{
+      // return axios.post('http://localhost:5000/api/model/test',{
+        "url":imageURL,
+        "option":option
+      })
+    } catch (error) {
+      console.error(error)
+    }
+}
+const modelProcess=async(urlImage,option,res)=>{
+  modelOutput(urlImage,option)
+    .then(response=>{
+      res.json({"diseaseResult":response.data});
+    })
+    .catch(error=>{
+      let out={'output':{
+        "msg":"Model: heroku Internal Server Error."
+      }};
+      res.json({"diseaseResult":out})
+    })
+}
+router.post('/uploadImage',(req,res)=>{
+  let option=req.query.option;
+  if (option==='') {
+    output={
+      imageUploadedStatus:'No parameter(option).'
+    }
+    res.json({
+      "output":output
+    });
+  }else{
+      var form = new formidable.IncomingForm();
+      form.encoding='utf-8';
+      form.keepExtensions=true;
+        form.parse(req, function (err, fields, files) {  
+          uploadImage(files).then((result)=>{
+            console.log("Image Upload to Cloudinary.");
+            output={
+              imageUploadedStatus:'Image Uploaded',
+              imageUploadedUrl:result.url,
+              imageUploadedOption:option
+            }
+            res.json({"output":output});
+            // modelProcess(result,option,res)
+          },(error)=>{
+            console.log(error);
+            output={
+              imageUploadedStatus:'Image Upload Failed.'
+            }
+            res.json({
+              "output":output
+            });
+          })
+        });
+  }
+});
+
+router.get('/model',(req,res)=>{
+  let option=req.query.option;
+  let urlIm=req.query.url;
+  if (option==='') {
+    let out={'output':{
+      "msg":"No paramter(option)"
+    }};
+    res.json({"diseaseResult":out});
+  }else{
+      modelProcess(urlIm,option,res)
+  }
+});
+
+module.exports=router;
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express=require('express');
-// const bodyParser=require('body-parser');
-// const cors=require('cors');
+const bodyParser=require('body-parser');
+const cors=require('cors');
 const app=express();
 const path=require('path');
 require('./config/config');
-// const imageTest=require('./routers/imageTest/imageTest');
+const imageTest=require('./routers/imageTest/imageTest');
 // const users=require('./routers/users/users');
 const port=process.env.PORT || 8086;
-// app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static(path.join(__dirname, 'build')));
-// // app.use('/api',imageTest);
+app.use('/api',imageTest);
 // // app.use('/users',users);
 
 app.get('*', (req, res) => {
