b9a050b75a4a011d2d31d9dcbcaeef4a7236ba52
sarru1291 <sarru1291@gmail.com>
2019-02-11 01:36:32 +0530

    build 1.0 served

---
+++ build/precache-manifest.94a14f91df7a9e74688de01f2f52931c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f3bd5ecdd7996997aa8",
+    "url": "/static/css/main.cc673008.chunk.css"
+  },
+  {
+    "revision": "7f3bd5ecdd7996997aa8",
+    "url": "/static/js/main.7f3bd5ec.chunk.js"
+  },
+  {
+    "revision": "d1a3854e4fac26f69881",
+    "url": "/static/js/1.d1a3854e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80a56f0c8c83b8b93e863e6c652478fe",
+    "url": "/static/media/icon.80a56f0c.png"
+  },
+  {
+    "revision": "4f2600ce78b2463fa4197b11dc104a3e",
+    "url": "/static/media/splashimage1.4f2600ce.jpg"
+  },
+  {
+    "revision": "510bb0bffa0b4c025be8dc2a53e3fbe9",
+    "url": "/static/media/camera.510bb0bf.png"
+  },
+  {
+    "revision": "20e7e689de73ced9ac850c45c96b3d21",
+    "url": "/static/media/skin.20e7e689.png"
+  },
+  {
+    "revision": "a9987c2763c3fa2380b02677c2527bb8",
+    "url": "/static/media/skincancer.a9987c27.txt"
+  },
+  {
+    "revision": "5cf485988e5129fad22d0a387e3c095a",
+    "url": "/static/media/retinopathy.5cf48598.txt"
+  },
+  {
+    "revision": "af29ede7a8fd60eb3a8faa1ca02a3754",
+    "url": "/static/media/paprika-regular.af29ede7.ttf"
+  },
+  {
+    "revision": "c1bf2487f760b6be190d75a877bf6f06",
+    "url": "/static/media/splashimage2.c1bf2487.jpeg"
+  },
+  {
+    "revision": "6e7c2aa6f3df7339aa96d21618010805",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 // const bodyParser=require('body-parser');
 // const cors=require('cors');
 const app=express();
-// const path=require('path');
+const path=require('path');
 // require('./config/config');
 // const imageTest=require('./routers/imageTest/imageTest');
 // const users=require('./routers/users/users');
@@ -10,12 +10,13 @@ const port=process.env.PORT || 8086;
 // app.use(cors());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended:true}));
-// // app.use(express.static(path.join(__dirname, '/build')));
+
+app.use(express.static(path.join(__dirname, 'build')));
 // // app.use('/api',imageTest);
 // // app.use('/users',users);
+
 app.get('*', (req, res) => {
-//   // res.sendFile(path.join(__dirname+'/build/index.html'));
-  res.send('hello');
+  res.sendFile(path.join(__dirname+'/build/index.html'));
 });
 
 app.listen(port,()=>{
