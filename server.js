4175562817580c9aaee67dcbfbd4f5fe6d789c21
sarru1291 <sarru1291@gmail.com>
2019-02-11 01:18:56 +0530

    1.0 heroku hosting

---
+++ server.js
@@ -0,0 +1,23 @@
+const express=require('express');
+// const bodyParser=require('body-parser');
+// const cors=require('cors');
+const app=express();
+// const path=require('path');
+// require('./config/config');
+// const imageTest=require('./routers/imageTest/imageTest');
+// const users=require('./routers/users/users');
+const port=process.env.PORT || 8086;
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}));
+// // app.use(express.static(path.join(__dirname, '/build')));
+// // app.use('/api',imageTest);
+// // app.use('/users',users);
+app.get('*', (req, res) => {
+//   // res.sendFile(path.join(__dirname+'/build/index.html'));
+  res.send('hello');
+});
+
+app.listen(port,()=>{
+  console.log(`server is running at port ${port}...`);
+});
